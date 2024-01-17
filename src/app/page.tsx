4aefd3186c75698e20fe61d389204c9843cccb62
devMiguelCarrero <developermiguelcarrero@gmail.com>
2024-01-17 11:49:32 -0500

    update basic app files
    remove base styles & update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,46 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import ButtonGrid, { GridItem } from '@/components/UI/ButtonGrid';
+import MainSection from '@/components/UI/MainSection';
+import Gaming3DButton from '@/buttons/Gaming3DButton/Gaming3DButton';
+import VegasNeonButton from '@/buttons/VegasNeonButton/VegasNeonButton';
+import SimpleDownloadButton from '@/buttons/SimpleDownloadButton/SimpleDownloadButton';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main>
+      <ButtonGrid>
+        <MainSection>
+          <GridItem>
+            <Gaming3DButton>OK!</Gaming3DButton>
+          </GridItem>
+          <GridItem>
+            <Gaming3DButton baseType="rounded" buttonType="danger">
+              X
+            </Gaming3DButton>
+          </GridItem>
+          <GridItem>
+            <Gaming3DButton buttonType="warning">warning</Gaming3DButton>
+          </GridItem>
+          <GridItem>
+            <Gaming3DButton buttonType="success">success</Gaming3DButton>
+          </GridItem>
+          <GridItem>
+            <Gaming3DButton disabled={true}>disabled</Gaming3DButton>
+          </GridItem>
+        </MainSection>
+        <MainSection backgroundColor="#030A1D">
+          <GridItem>
+            <VegasNeonButton>OK!</VegasNeonButton>
+          </GridItem>
+          <GridItem>
+            <VegasNeonButton buttonType="intermitent">OK!</VegasNeonButton>
+          </GridItem>
+        </MainSection>
+        <MainSection backgroundColor="#FAF4E4">
+          <GridItem>
+            <SimpleDownloadButton>DOWNLOAD</SimpleDownloadButton>
+          </GridItem>
+        </MainSection>
+      </ButtonGrid>
     </main>
-  )
+  );
 }
